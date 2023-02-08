f97993d0fc339ee84a886539edb8aa8909a88df6
Jay Roebuck <jroebuck@esri.com>
2023-02-08 15:02:53 -0500

    add execute log archiver gh action

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 import arcgis
-import concurrent.futures
 from arcgis.gis import GIS
 from arcgis.gis.admin import PortalAdminManager
+
+import concurrent.futures
 import datetime as _dt
 from typing import Any
-
-def handler(event, context):
-    message = 'Hello from AWS Lambda using ArcGIS API for Python ' + arcgis.__version__ + '!'
-    if event.get('action', 'hello_world') == 'hello_world':
-        return message
-    if event.get('action') == 'echo':
-        return event
-
-    gis:GIS = get_gis(event)
-    logs_by_server = get_logs(gis)
-    return logs_by_server
+from pprint import pprint
 
 def get_gis(event):
     return GIS(url=event.get('url'), username=event.get('username'), password=event.get('password'))
@@ -68,3 +59,35 @@ def get_logs(gis):
                 server_url = params.pop('server')
                 servers[server_url].extend(ref[server_url].logs.query(**params))
         return servers
+
+def handler(event, context):
+    """
+    aws lambda handler
+    """
+    message = 'Hello from AWS Lambda using ArcGIS API for Python ' + arcgis.__version__ + '!'
+    if event.get('action', 'hello_world') == 'hello_world':
+        return message
+    if event.get('action') == 'echo':
+        return event
+
+    gis:GIS = get_gis(event)
+    logs_by_server = get_logs(gis)
+    return logs_by_server
+
+def main():
+    import sys
+    args = sys.argv[-3:] # last 3 args
+    try:
+        url, username, password = args
+        if not url.startswith('http'): raise ValueError()
+    except ValueError:
+        print('Please pass url, username, password as ordered args')
+        sys.exit(1)
+    gis:GIS = GIS(url=url, username=username, password=password)
+    logs_by_server = get_logs(gis)
+    result = logs_by_server
+    pprint(logs_by_server)
+
+
+if __name__ == '__main__':
+    main()
